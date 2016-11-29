a87f17f00221f10055f95731d1b5fd7b4bae313f
RidwanAlam <RidwanAlam@users.noreply.github.com>
2016-11-29 10:59:27 -0500

    Update install.sh

--- LED_Control/install.sh
+++ LED_Control/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -f TurnOffLeds.sh /usr/bin/TurnOffLeds.sh
-cp -f LEDOff.service /lib/systemd/system/LEDOff.service
+cp -f ~/besi-relay-station/LED_Control/TurnOffLeds.sh /usr/bin/TurnOffLeds.sh
+cp -f ~/besi-relay-station/LED_Control/LEDOff.service /lib/systemd/system/LEDOff.service
 chmod +x /usr/bin/TurnOffLeds.sh
 
 systemctl daemon-reload
