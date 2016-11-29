604a275f9fc7e2e69113570b3b70a60c3478c5f3
RidwanAlam <RidwanAlam@users.noreply.github.com>
2016-11-28 21:14:23 -0500

    Update initSD.sh

--- SD_card/initSD.sh
+++ SD_card/initSD.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir /media/card
 mount -v /dev/mmcblk1p1 /media/card
+cp uEnv.txt /media/card/uEnv.txt
+sed -i '$ a /dev/mmcblk0p1 /media/card auto auto,rw,async,user,nofail 0 0' /etc/fstab
+reboot
